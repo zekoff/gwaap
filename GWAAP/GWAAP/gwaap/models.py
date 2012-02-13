b4c60870f0fce998e193917150a4e2cc8c00f126
Michael Zekoff <zekoff@gmail.com>
2012-02-12 21:26:42 -0600

    Work begun on ApplicantProfile class. Currently includes some passing and some failing test cases.
    Started a second version of the documentation spreadsheet.

--- GWAAP/GWAAP/gwaap/models.py
+++ GWAAP/GWAAP/gwaap/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
 class User(User):
@@ -10,4 +11,16 @@ class Applicant(User):
 
 class Application(models.Model):
     pass
-    applicant = models.ForeignKey(Applicant)
+
+class ApplicantProfile(models.Model):
+    user = models.OneToOneField(Applicant)
+#    application = models.OneToOneField(Application)
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        # Was using ApplicantProfile.objects.get(user=instance)
+        # This wasn't working, so changed to get_or_create from a StackOverflow answer
+        # http://stackoverflow.com/questions/6388105/django-integrityerror-column-user-id-is-not-unique
+        ApplicantProfile.objects.get_or_create(user=instance)
+        
+post_save.connect(create_user_profile, sender=Applicant)
--- GWAAP/GWAAP/gwaap/tests.py
+++ GWAAP/GWAAP/gwaap/tests.py
@@ -9,6 +9,8 @@ from django.test import TestCase
 from models import User
 from models import Applicant
 from models import Application
+from GWAAP.gwaap.models import ApplicantProfile
+import django.db.models
 
 #class SimpleTest(TestCase):
 #    def test_basic_addition(self):
@@ -26,6 +28,11 @@ class ModelTests(TestCase):
     def test_0020_userHasUsername(self):
         user = User()
         self.assertEqual(user.username, '')
+        
+    def test_0021_setUsername(self):
+        user = User()
+        user.username = "jimbob"
+        self.assertEqual(user.username, "jimbob")
         
     def test_0030_userNameWorks(self):
         user = User()
@@ -48,15 +55,39 @@ class ModelTests(TestCase):
         
 #    def test_0070_applicationHasNameField(self):
 #        app = Application()
-#        name = app.name
-#        print name
-#        self.assertEqual(name, "")
+#        app.first_name = "Joe"
+#        app.save()
+#        self.assertEqual(app.first_name, "Joe")
 
-    def test_0071_applicationHasApplicantForeignKey(self):
-        app = Application()
+#    def test_0071_applicationHasApplicantForeignKey(self):
+#        app = Application()
+#        applicant = Applicant()
+#        applicant.save()
+#        app.owner = applicant
+#        app.save()
+#        app = Application.objects.get(pk=1)
+#        self.assertEqual(applicant, app.owner)
+        
+    def test_0080_applicantProfileExists(self):
+        applicantProfile = ApplicantProfile()
+        self.assertIsInstance(applicantProfile, ApplicantProfile)
+        
+    def test_0090_applicantProfileIsModel(self):
+        ap = ApplicantProfile()
+        self.assertIsInstance(ap, django.db.models.Model)
+        
+    def test_0100_applicantHasProfile(self):
         applicant = Applicant()
         applicant.save()
-        app.owner = applicant
-        app.save()
-        app = Application.objects.get(pk=1)
-        self.assertEqual(applicant, app.owner)
+        self.assertIsInstance(applicant.get_profile(), ApplicantProfile)
+        
+    def test_0111_applicantHasProfileTwoCopies(self):
+        applicant = Applicant()
+        applicant.save()
+        applicant2=Applicant()
+        applicant2.save()
+        self.assertIsInstance(applicant2.get_profile(), ApplicantProfile)
+        
+    def test_0110_applicantProfileHasApplication(self):
+        ap = ApplicantProfile()
+        self.assertIsInstance(ap.application, Application)
