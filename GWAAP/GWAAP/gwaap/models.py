551ece37b28448a7a87368468c2a9ad7c5fd5410
Michael Zekoff <zekoff@gmail.com>
2012-02-19 13:15:41 -0600

    ApplicantProfile tests working.

--- GWAAP/GWAAP/gwaap/models.py
+++ GWAAP/GWAAP/gwaap/models.py
@@ -1,6 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db import models
+#from django.db.models.signals import post_save, pre_save
 
 # Create your models here.
 class User(User):
@@ -13,14 +13,19 @@ class Application(models.Model):
     pass
 
 class ApplicantProfile(models.Model):
-    user = models.OneToOneField(Applicant)
+    user = models.ForeignKey(Applicant, unique=True)
 #    application = models.OneToOneField(Application)
 
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        # Was using ApplicantProfile.objects.get(user=instance)
-        # This wasn't working, so changed to get_or_create from a StackOverflow answer
-        # http://stackoverflow.com/questions/6388105/django-integrityerror-column-user-id-is-not-unique
-        ApplicantProfile.objects.get_or_create(user=instance)
-        
-post_save.connect(create_user_profile, sender=Applicant)
+#def create_user_profile(sender, instance, created, **kwargs):
+#    if created:
+#        pass
+#        # Was using ApplicantProfile.objects.get(user=instance)
+#        # This wasn't working, so changed to get_or_create from a StackOverflow answer
+#        # http://stackoverflow.com/questions/6388105/django-integrityerror-column-user-id-is-not-unique
+##        ApplicantProfile.objects.get_or_create(user=instance)
+#        ApplicantProfile.objects.create(user=instance)
+##        ap = ApplicantProfile(user=instance)
+##        ap.save()
+#        
+#post_save.connect(create_user_profile, sender=Applicant)
+## See http://www.turnkeylinux.org/blog/django-profile for another method of doing this
--- GWAAP/GWAAP/gwaap/tests.py
+++ GWAAP/GWAAP/gwaap/tests.py
@@ -3,14 +3,14 @@ This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
+
 """
 
-from django.test import TestCase
-from models import User
-from models import Applicant
-from models import Application
 from GWAAP.gwaap.models import ApplicantProfile
+from django.test import TestCase
+from models import Applicant, Application, User
 import django.db.models
+from django.db.utils import IntegrityError
 
 #class SimpleTest(TestCase):
 #    def test_basic_addition(self):
@@ -21,35 +21,35 @@ import django.db.models
 
 class ModelTests(TestCase):
     
-    def test_0010_userExists(self):
+    def test_00010_userExists(self):
         user = User()
         self.assertIsInstance(user, User)
     
-    def test_0020_userHasUsername(self):
+    def test_00020_userHasUsername(self):
         user = User()
         self.assertEqual(user.username, '')
         
-    def test_0021_setUsername(self):
+    def test_00021_setUsername(self):
         user = User()
         user.username = "jimbob"
         self.assertEqual(user.username, "jimbob")
         
-    def test_0030_userNameWorks(self):
+    def test_00030_userNameWorks(self):
         user = User()
         user.first_name = "Joe"
         user.last_name = "Tester"
         self.assertEqual(user.get_full_name(), "Joe Tester")
 
-    def test_0040_userPassword(self):
+    def test_00040_userPassword(self):
         user = User()
         user.set_password("password42")
         self.assertTrue(user.check_password("password42"))
         
-    def test_0050_applicantExists(self):
+    def test_00050_applicantExists(self):
         applicant = Applicant()
         self.assertIsInstance(applicant, Applicant)
         
-    def test_0060_applicationExists(self):
+    def test_00060_applicationExists(self):
         app = Application()
         self.assertIsInstance(app, Application)
         
@@ -68,26 +68,107 @@ class ModelTests(TestCase):
 #        app = Application.objects.get(pk=1)
 #        self.assertEqual(applicant, app.owner)
         
-    def test_0080_applicantProfileExists(self):
+    def test_00080_applicantProfileExists(self):
         applicantProfile = ApplicantProfile()
         self.assertIsInstance(applicantProfile, ApplicantProfile)
         
-    def test_0090_applicantProfileIsModel(self):
+    def test_00090_applicantProfileIsModel(self):
         ap = ApplicantProfile()
         self.assertIsInstance(ap, django.db.models.Model)
         
-    def test_0100_applicantHasProfile(self):
-        applicant = Applicant()
+    def test_00091_applicantProfileHasId(self):
+        app = Applicant(username="applicant")
+        app.save()
+        ap = ApplicantProfile(user=app)
+        ap.save()
+        apID = ap.id
+        self.assertEqual(apID, 1)
+        
+    def test_00100_applicantHasProfile(self):
+        applicant = Applicant.objects.create()
+        appProfile = ApplicantProfile(user=applicant)
+        appProfile.save()
         applicant.save()
         self.assertIsInstance(applicant.get_profile(), ApplicantProfile)
         
-    def test_0111_applicantHasProfileTwoCopies(self):
-        applicant = Applicant()
-        applicant.save()
-        applicant2=Applicant()
-        applicant2.save()
-        self.assertIsInstance(applicant2.get_profile(), ApplicantProfile)
+    def test_00101_gettingIdOfApplicant(self):
+        app = Applicant()
+        app.save()
+        appID = app.id
+        self.assertEqual(appID, 1)
         
-    def test_0110_applicantProfileHasApplication(self):
-        ap = ApplicantProfile()
-        self.assertIsInstance(ap.application, Application)
+#    def test_0102_gettingIdOfSecondApplicant(self):
+#        app = Applicant.objects.create()
+#        app.save()
+#        app2 = Applicant.objects.create()
+#        app2.save()
+#        appID = app2.id
+#        self.assertEqual(appID, 2)
+        
+        
+#This test really isn't asking anything I want to ask
+#    def test_0110_applicantProfileHasApplication(self):
+#        ap = ApplicantProfile()
+#        self.assertIsInstance(ap.get_profile(), Application)
+
+#    def test_0111_applicantHasProfileTwoCopies(self):
+#        applicant3 = Applicant.objects.create()
+#        applicant3.save()
+#        applicant2 = Applicant.objects.create()
+#        applicant2.save()
+#        self.assertIsInstance(applicant2.get_profile(), ApplicantProfile)
+
+    def test_00120_makeTwoApplicantsWithProfiles(self):
+        applicant = Applicant(username="mr. applicant")
+        applicant.save()
+        ap = ApplicantProfile(user=applicant)
+        ap.save()
+        applicant.save()
+        applicant2 = Applicant(username="mrs. applicant")
+        applicant2.save()
+        ap2 = ApplicantProfile(user=applicant2)
+        ap2.save()
+        applicant2.save()
+        self.assertEqual(ap2.id, 2)
+        
+    def test_00130_makeSingleApplicant(self):
+        applicant = Applicant(username="applicant")
+        applicant.save()
+        ap = ApplicantProfile(user=applicant)
+        ap.save()
+        applicant.save()
+        self.assertIsInstance(applicant, Applicant)
+
+    def test_00140_getApplicantById(self):
+        applicant = Applicant(username="applicant")
+        applicant.save()
+        ap = ApplicantProfile(user=applicant)
+        ap.save()
+        applicant.save()
+        appKey = Applicant.objects.get(username="applicant").pk
+        self.assertEqual(appKey, 1)
+        
+    def test_00150_getApplicantByIdMultipleApplicants(self):
+        applicant = Applicant(username="applicant")
+        applicant.save()
+        ap = ApplicantProfile(user=applicant)
+        ap.save()
+        applicant.save()
+        applicant = Applicant(username="applicant2")
+        applicant.save()
+        ap = ApplicantProfile(user=applicant)
+        ap.save()
+        applicant.save()
+        applicant = Applicant(username="applicant3")
+        applicant.save()
+        ap = ApplicantProfile(user=applicant)
+        ap.save()
+        applicant.save()
+        appKey = Applicant.objects.get(username="applicant2").pk
+        self.assertEqual(2, appKey)
+
+    def test_00160_duplicateUsernamesRaisesError(self):
+        applicant = Applicant(username="applicant")
+        applicant.save()
+        applicant = Applicant(username="applicant")
+        self.assertRaises(IntegrityError, applicant.save)
